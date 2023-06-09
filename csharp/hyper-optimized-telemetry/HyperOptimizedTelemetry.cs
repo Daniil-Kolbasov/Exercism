9254037a417edf9eec5f5ef58394ce62bf41555e
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-10 01:04:02 +0500

    Update Hyper-optimized Telemetry
    dzegel's solution

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -1,79 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class TelemetryBuffer
 {
 	public static byte[] ToBuffer(long reading)
 	{
-		List<byte> buffer = new();
-		byte[] bytes;
-
-		switch (reading)
+		var bytes = reading switch
 		{
-			// long: from -9_223_372_036_854_775_808 to -2_147_483_649
-			case < int.MinValue:
-				bytes = BitConverter.GetBytes(reading);
-				buffer.Add((byte)(256 - bytes.Length));
-				buffer.AddRange(bytes);
-				break;
-			// int: from -2_147_483_648 to -32_769
-			case < short.MinValue:
-				bytes = BitConverter.GetBytes((int)reading);
-				buffer.Add((byte)(256 - bytes.Length));
-				buffer.AddRange(bytes);
-				break;
-			// short: from -32_768 to -1
-			case < 0:
-				bytes = BitConverter.GetBytes((short)reading);
-				buffer.Add((byte)(256 - bytes.Length));
-				buffer.AddRange(bytes);
-				break;
-			// ushort: from 0 to 65_535
-			case <= ushort.MaxValue:
-				bytes = BitConverter.GetBytes((ushort)reading);
-				buffer.Add((byte)bytes.Length);
-				buffer.AddRange(bytes);
-				break;
-			// int: from 65_536 to 2_147_483_647
-			case <= int.MaxValue:
-				bytes = BitConverter.GetBytes((int)reading);
-				buffer.Add((byte)(256 - bytes.Length));
-				buffer.AddRange(bytes);
-				break;
-			// uint: from 2_147_483_648 to 4_294_967_295
-			case <= uint.MaxValue:
-				bytes = BitConverter.GetBytes((uint)reading);
-				buffer.Add((byte)bytes.Length);
-				buffer.AddRange(bytes);
-				break;
-			// log: from 4_294_967_296 to 9_223_372_036_854_775_807
-			default:
-				bytes = BitConverter.GetBytes(reading);
-				buffer.Add((byte)(256 - bytes.Length));
-				buffer.AddRange(bytes);
-				break;
-		}
-
-		while (buffer.Count < 9)
-		{
-			buffer.Add(0);
-		}
-
-		return buffer.ToArray();
-	}
-
-	public static long FromBuffer(byte[] buffer)
-	{
-		int prefix = buffer[0] == 4 || buffer[0] == 2 ? buffer[0] : buffer[0] - 256;
-
-		return (prefix) switch
-		{
-			-8 => BitConverter.ToInt64(buffer.AsSpan()[1..]),
-			-4 => BitConverter.ToInt32(buffer.AsSpan()[1..]),
-			-2 => BitConverter.ToInt16(buffer.AsSpan()[1..]),
-			2 => BitConverter.ToUInt16(buffer.AsSpan()[1..]),
-			4 => BitConverter.ToUInt32(buffer.AsSpan()[1..]),
-			_ => 0,
+			< Int32.MinValue => BitConverter.GetBytes((long)reading).Prepend((byte)(256 - 8)),
+			< Int16.MinValue => BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 4)),
+			< UInt16.MinValue => BitConverter.GetBytes((short)reading).Prepend((byte)(256 - 2)),
+			<= UInt16.MaxValue => BitConverter.GetBytes((ushort)reading).Prepend((byte)2),
+			<= Int32.MaxValue => BitConverter.GetBytes((int)reading).Prepend((byte)(256 - 4)),
+			<= UInt32.MaxValue => BitConverter.GetBytes((uint)reading).Prepend((byte)4),
+			_ => BitConverter.GetBytes((long)reading).Prepend((byte)(256 - 8)),
 		};
+		return bytes.Concat(new byte[9 - bytes.Count()]).ToArray();
 	}
+
+	public static long FromBuffer(byte[] buffer) => buffer[0] switch
+	{
+		256 - 8 or 4 or 2 => BitConverter.ToInt64(buffer, 1),
+		256 - 4 => BitConverter.ToInt32(buffer, 1),
+		256 - 2 => BitConverter.ToInt16(buffer, 1),
+		_ => 0,
+	};
 }
