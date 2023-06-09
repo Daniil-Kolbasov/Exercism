386c71994b70c39cd60efb9b4dd46be064c2b3f6
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-10 01:18:02 +0500

    Add Annalyn's Infiltration

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,42 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        if (knightIsAwake)
+            return false;
+        else
+            return true;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (archerIsAwake == false && prisonerIsAwake == true)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent)
+            if (archerIsAwake == false)
+                return true;
+            else
+                return false;
+        else
+            if (knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true)
+                return true;
+            else
+                return false;
+    }
+}
