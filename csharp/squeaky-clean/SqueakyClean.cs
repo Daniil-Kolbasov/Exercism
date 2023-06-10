640800f01eeb1367e5dd73189f2a2c514eae0574
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-10 15:20:37 +0500

    Update Squeaky Clean
    danieldietrich's solution

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,32 +1,25 @@
-using System;
-using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 
 public static class Identifier
 {
-    public static string Clean(string identifier)
-    {
-        bool isUpper = false;
-        char[] temp = identifier.ToCharArray();
-
-        for (int i = 0; i < temp.Length; i++)
-        {
-            if (isUpper)
-            {
-                temp[i] = Char.ToUpper(temp[i]);
-                isUpper = false;
-            }
-
-            if (temp[i] == '-')
-                isUpper = true;
-        }
-
-        identifier = new(temp);
-
-        return new(identifier
-                   .Replace(" ", "_")
-                   .Replace("\0", "CTRL")
-                   .Where(c => char.IsLetter(c) && !(c >= '\u03B1' && c <= '\u03C9') || c == '_')
-                   .ToArray());
-    }
+	private static bool IsGreekLowercase(char c) => (c >= 'α' && c <= 'ω');
+	public static string Clean(string identifier)
+	{
+		var stringBuilder = new StringBuilder();
+		var isAfterDash = false;
+		foreach (var c in identifier)
+		{
+			stringBuilder.Append(c switch
+			{
+				_ when IsGreekLowercase(c) => default,
+				_ when isAfterDash => char.ToUpperInvariant(c),
+				_ when char.IsWhiteSpace(c) => '_',
+				_ when char.IsControl(c) => "CTRL",
+				_ when char.IsLetter(c) => c,
+				_ => default,
+			});
+			isAfterDash = c.Equals('-');
+		}
+		return stringBuilder.ToString();
+	}
 }
