3f5586e387c4838896552d97d65dbea3e72937f9
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-27 21:17:26 +0500

    Update Tracks on Tracks on Tracks

--- csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
+++ csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -5,26 +5,23 @@ using System.Linq;
 
 public static class Languages
 {
-	public static List<string> NewList() => new();
+	public static List<string> NewList() => 
+		new();
 
-	public static List<string> GetExistingLanguages() => new() { "C#", "Clojure", "Elm" };
+	public static List<string> GetExistingLanguages() => 
+		new() { "C#", "Clojure", "Elm" };
 
-	public static List<string> AddLanguage(List<string> languages, string language)
-	{
-		languages.Add(language);
-		return languages;
-	}
+	public static List<string> AddLanguage(List<string> languages, string language) =>
+		languages.Append(language).ToList();
 
-	public static int CountLanguages(List<string> languages) => languages.Count;
+	public static int CountLanguages(List<string> languages) => 
+		languages.Count;
 
 	public static bool HasLanguage(List<string> languages, string language) =>
 		languages.Contains(language);
 
-	public static List<string> ReverseList(List<string> languages)
-	{
-		languages.Reverse();
-		return languages;
-	}
+	public static List<string> ReverseList(List<string> languages) => 
+		languages.Reverse<string>().ToList();
 
 	public static bool IsExciting(List<string> languages) => 
 		(languages.FirstOrDefault() ?? string.Empty) == "C#" || 
