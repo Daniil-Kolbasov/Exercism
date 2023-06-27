c55908bf488f49df4184e2bf94c675dcec35c51d
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-27 16:51:33 +0500

    Update Log Levels

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -2,40 +2,27 @@ using System;
 
 static class LogLine
 {
-    private static string[] LogLevelArray = { "[INFO]:", "[WARNING]:", "[ERROR]:" };
+	private static string[] LogLevelArray = { "[INFO]:", "[WARNING]:", "[ERROR]:" };
 
-        public static string Message(string logLine)
-        {
-            return message(logLine);
-        }
+	public static string Message(string logLine) => message(logLine);
 
-        public static string LogLevel(string logLine)
-        {
-            
-            return logLevel(logLine);
-        }
+	public static string LogLevel(string logLine) => logLevel(logLine);
 
-        public static string Reformat(string logLine)
-        {
-            return message(logLine) + " (" + logLevel(logLine) + ")";
-        }
+	public static string Reformat(string logLine) => message(logLine) + " (" + logLevel(logLine) + ")";
 
-        private static string logLevel(string logLine)
-        {
-            foreach (var item in LogLevelArray)
-            {
-                if (logLine.Contains(item))
-                {
-                    return item.TrimStart('[').TrimEnd(']',':').ToLower();
-                }
-            }
+	private static string logLevel(string logLine)
+	{
+		foreach (var item in LogLevelArray)
+		{
+			if (logLine.Contains(item))
+			{
+				return item.TrimStart('[').TrimEnd(']', ':').ToLower();
+			}
+		}
 
-            // TODO: throw exception
-            return logLine;
-        }
+		return logLine;
+	}
 
-        private static string message(string logLine)
-        {
-            return logLine.Remove(0, logLevel(logLine).Length + 3).Trim(' ', '\r', '\n', '\t');
-        }
+	private static string message(string logLine) => 
+		logLine.Remove(0, logLevel(logLine).Length + 3).Trim(' ', '\r', '\n', '\t');
 }
