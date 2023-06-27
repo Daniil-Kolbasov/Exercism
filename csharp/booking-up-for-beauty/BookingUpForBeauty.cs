c89d86c3354551d5038d9b10d735da39fed1229b
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-27 16:18:10 +0500

    Update Booking up for Beauty

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -3,31 +3,26 @@ using System.Globalization;
 
 static class Appointment
 {
-    public static DateTime Schedule(string appointmentDateDescription)
-    {
-        if (DateTime.TryParse(appointmentDateDescription, out var date))
-            return date;
-        else if (DateTime.TryParseExact(appointmentDateDescription, "M/d/yyyy HH:mm:ss",
-            new CultureInfo("en-US"), DateTimeStyles.None, out var dateValue))
-            return dateValue;
+	public static DateTime Schedule(string appointmentDateDescription)
+	{
+		if (DateTime.TryParse(appointmentDateDescription, out var date))
+			return date;
+		else if (DateTime.TryParseExact(appointmentDateDescription, "M/d/yyyy HH:mm:ss",
+			new CultureInfo("en-US"), DateTimeStyles.None, out var dateValue))
+			return dateValue;
 
-        throw new FormatException($"String '{appointmentDateDescription}' was not recognized as a valid DateTime");
-    }
+		throw new FormatException($"String '{appointmentDateDescription}' was not recognized as a valid DateTime");
+	}
 
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        var currentDate = DateTime.Now;
-        int result = DateTime.Compare(currentDate, appointmentDate);
+	public static bool HasPassed(DateTime appointmentDate) => 
+		DateTime.Compare(DateTime.Now, appointmentDate) > 0;
 
-        if (result > 0)
-            return true;
-        
-        return false;
-    }
+	public static bool IsAfternoonAppointment(DateTime appointmentDate) => 
+		appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
 
-	public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+	public static string Description(DateTime appointmentDate) => 
+		$"You have an appointment on {appointmentDate.ToString(new CultureInfo("en_US"))}.";
 
-	public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString(new CultureInfo("en_US"))}.";
-
-	public static DateTime AnniversaryDate() => new(DateTime.Now.Year, 9, 15);
+	public static DateTime AnniversaryDate() => 
+		new(DateTime.Now.Year, 9, 15);
 }
