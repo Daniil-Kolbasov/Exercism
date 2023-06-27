f13082bb6785ad9f2e9e8534385fcfc1980e3947
Daniil Kolbasov <34882813+Daniil-Kolbasov@users.noreply.github.com>
2023-06-27 16:14:21 +0500

    Add Booking up for Beauty

---
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        if (DateTime.TryParse(appointmentDateDescription, out var date))
+            return date;
+        else if (DateTime.TryParseExact(appointmentDateDescription, "M/d/yyyy HH:mm:ss",
+            new CultureInfo("en-US"), DateTimeStyles.None, out var dateValue))
+            return dateValue;
+
+        throw new FormatException($"String '{appointmentDateDescription}' was not recognized as a valid DateTime");
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        var currentDate = DateTime.Now;
+        int result = DateTime.Compare(currentDate, appointmentDate);
+
+        if (result > 0)
+            return true;
+        
+        return false;
+    }
+
+	public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+
+	public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString(new CultureInfo("en_US"))}.";
+
+	public static DateTime AnniversaryDate() => new(DateTime.Now.Year, 9, 15);
+}
